be5c2de0e7021a87a82b7bea86cd29e1ab5a1e8c
the-phoenix-coder <ahmedhesham.dev@gmail.com>
2026-04-28 20:06:43 +0300

    style: added animation for hover effect on button and added dark mood toggle button and developed it's features

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
 const btn = document.querySelector('#menu')
-    btn.addEventListener('click', () => {
-        const menuContainer = document.querySelector('#menu-container')        
-        // التبديل بين الحركة من فوق لتحت
-        menuContainer.classList.toggle('invisible');
-        menuContainer.classList.toggle('-translate-x-full');
-        menuContainer.classList.toggle('translate-x-0');
-        
-        // مهم جداً عشان الأزرار متتضغطش وهي مخفية
-        menuContainer.classList.toggle('pointer-events-none')
-    })
+btn.addEventListener('click', () => {
+    const menuContainer = document.querySelector('#menu-container')        
+    // التبديل بين الحركة من فوق لتحت
+    menuContainer.classList.toggle('invisible');
+    menuContainer.classList.toggle('-translate-x-full');
+    menuContainer.classList.toggle('translate-x-0');
+    
+    // مهم جداً عشان الأزرار متتضغطش وهي مخفية
+    menuContainer.classList.toggle('pointer-events-none')
+})
+// DARK AND DAY MODE TOGGLE
+const themeToggle = document.querySelector('#themeToggle')
+
+if (localStorage.getItem('theme') === 'dark')
+{
+    document.documentElement.classList.add('dark')
+    themeToggle.querySelector('img').src = './Images/sun.png'
+}
+
+themeToggle.addEventListener('click', () =>
+{
+    document.documentElement.classList.toggle('dark')
+    if (document.documentElement.classList.contains('dark'))
+    {
+        localStorage.setItem('theme', 'dark')
+        themeToggle.querySelector('img').src = './Images/sun.png'
+    }
+    else
+    {
+        localStorage.setItem('theme', 'light')
+        themeToggle.querySelector('img').src = './Images/moon.png'
+    }
+})
